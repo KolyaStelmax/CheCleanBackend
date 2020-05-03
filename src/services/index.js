8f5537843addf0d50a91ea175ebcba477e199451
Dima Bilozor <swatdimonbil@gmail.com>
2020-05-03 18:37:10 +0300

    CC-49 added exports to services instance

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,8 @@
 import { CasesService } from './cases.service.js';
 import { PostgresService } from './postgres.service.js';
 
-const postgresService = new PostgresService();
-const casesService = new CasesService(postgresService);
+export const postgresService = new PostgresService();
+export const casesService = new CasesService(postgresService);
 
 export * from './cases.service.js';
 export * from './postgres.service.js';
