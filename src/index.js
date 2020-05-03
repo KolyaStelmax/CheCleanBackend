7d35d8d76ab6d3c8be59316acb6e38ec5dee690d
Dima Bilozor <swatdimonbil@gmail.com>
2020-05-03 17:48:47 +0300

    CC-1 commit bug fix

--- src/index.js
+++ src/index.js
@@ -11,15 +11,16 @@ app.get('/cases', getCases);
 
 app.use(bodyParser.json());
 app.use(
- bodyParser.urlencoded({
-  extended: true,
- }),
+  bodyParser.urlencoded({
+    extended: true,
+  }),
 );
 
 app.get('/', (request, response) => {
- response.json({ info: 'Node.js, Express, and Postgres API' });
+  response.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
 app.listen(process.env.PORT, () => {
- console.log(`Server started on port ${process.env.PORT}.`);
+  console.log(`Server started on port ${process.env.PORT}.`);
 });
+//
