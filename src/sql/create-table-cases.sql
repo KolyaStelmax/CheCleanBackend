87711ab063626555d45e3e2f8495e4195b60cbbc
Alexander Kretov <alexander.kretov99@gmail.com>
2020-05-03 20:41:57 +0300

    CC-1 updated the cases table

--- src/sql/create-table-cases.sql
+++ src/sql/create-table-cases.sql
@@ -1,13 +1,15 @@
 CREATE TABLE cases (
     id SERIAL NOT NULL PRIMARY KEY,
-    details VARCHAR(300) NOT NULL,
-    longtitude FLOAT NOT NULL,
-    latitude FLOAT NOT NULL,
-    photo_url VARCHAR(300) NOT NULL,
+    details VARCHAR(500) NOT NULL,
+    "location" POINT NOT NULL,
+    "address" VARCHAR(300) NOT NULL,
+    image_url VARCHAR(500) NOT NULL,
+    map_image_url VARCHAR(500) NOT NULL,
     "status" INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT NOW() 
 );
 
 -- TODO remove this
-INSERT INTO cases (details, longtitude, latitude, photo_url) 
-VALUES('мусор', 49.457857, 32.043704, 'test-url');
+INSERT INTO cases (details, "location", "address", image_url, map_image_url) 
+VALUES('мусор', POINT(49.457857, 32.043704), 'test_address', 'test-image', 'test-url');
+
