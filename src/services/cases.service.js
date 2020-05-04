44e94d8dded9bd55ab7de052163e78b3acec4369
Alexander Kretov <alexander.kretov99@gmail.com>
2020-05-04 22:22:46 +0300

    CC-1 connected cases service methods with postgres service

--- src/services/cases.service.js
+++ src/services/cases.service.js
@@ -3,25 +3,49 @@ export class CasesService {
     this.postgresService = postgresService;
   }
 
+  remapCase({id, details, location, address, image_url, map_image_url, status, created_at}) {
+    return {
+      id,
+      details,
+      location:{
+        latitude: location.x,
+        longtitude: location.y,
+        address,
+        map_image_url
+      },
+      image_url,
+      status,
+      created_at
+    }
+  }
+
   async getCases(searchParams) {
-    const casesRespose = await this.postgresService.pool.query('SELECT * FROM cases ORDER BY id ASC');
-    return casesRespose.rows;
-    return Promise.resolve([]);
+    const casesRespose = await this.postgresService.knex('cases').orderBy('id');
+    // console.log(casesRespose);
+    return casesRespose.map(this.remapCase)
   }
 
-  getCaseById(id) {
-    return Promise.resolve({});
+  async getCaseById(id) {
+    const idResponse = await this.postgresService.knex('cases').where('id', id);
+    return this.remapCase(idResponse[0]);
   }
 
-  createCase(newCase) {
-    return Promise.resolve({});
+  async createCase(newCase) {
+    const databaseCase = {
+      ...newCase
+    }
+    console.log(databaseCase);
+    const createdCase = await this.postgresService.knex('cases').insert(newCase);
+    return createdCase[0];
   }
 
-  resolveCase(id) {
-    return Promise.resolve(id);
+  async resolveCase(id) {
+    const resolvedCase = await this.knex('cases').where('id', id).update({status: 1});
+    return resolvedCase.rows
   }
 
   unresolveCase(id) {
-    return Promise.resolve(id);
+    // const resolvedCase = await this.knex('cases').where('id', id).update({status: 0});
+    // return resolvedCase.rows
   }
 }
