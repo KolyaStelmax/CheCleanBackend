86addacb5ef0e6f74f273dbeed83b540b8cc681a
Dima Bilozor <swatdimonbil@gmail.com>
2020-05-08 11:31:42 +0300

    CC-30 added all express endpoints

--- src/index.js
+++ src/index.js
@@ -18,21 +18,39 @@ app.get('/', (request, response) => {
   response.json({ info: 'CheClean' });
 });
 
+app.use((error, request, response, next) => {
+  request.status(500).json({ error });
+});
+
 app.get('/cases', async (request, response) => {
-  const cases = await casesService.getCases(request.query);
-  response.status(200).json(cases);
+  try {
+    const cases = await casesService.getCases(request.query);
+    response.status(200).json(cases);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get('/cases/:id', async (request, response) => {
+  const caseById = await casesService.getCaseById(+request.params.id);
+  response.status(200).json(caseById);
+});
+
+app.post('/cases', async (request, response) => {
+  const createdCase = await casesService.createCase(request.body);
+  response.status(200).json(createdCase);
+});
+
+app.patch('/cases/:id/resolve', async (request, response) => {
+  const resolveCase = await casesService.resolveCase(+request.params.id);
+  response.status(200).json(resolveCase);
+});
+
+app.patch('/cases/:id/unresolve', async (request, response) => {
+  const unresolveCase = await casesService.unresolveCase(+request.params.id);
+  response.status(200).json(unresolveCase);
 });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}.`);
 });
-
-let newCase = {
-  details: 'мусор',
-  location: { longitude: 49.457857, latitude: 32.043704 },
-  image_url: 'test image',
-};
-
-// casesService.getCaseById(3).then(console.log);
-// // casesService.createCase(newCase).then(console.log);
-// casesService.unresolveCase(3).then(console.log);
