3df7f862f86af144f60da630987e83ac4397a1ef
Alexander Kretov <alexander.kretov99@gmail.com>
2020-05-14 18:49:52 +0300

    CC-15 fixed the create_cases_table migration

--- migrations/20200512183034_create_cases_table.js
+++ migrations/20200512183034_create_cases_table.js
@@ -3,15 +3,14 @@ const knex = require('knex');
 /**
  * @param {knex} knex
  */
-
 exports.up = function(knex) {
-  return knex.schema.hasTable('cases', (exists) =>{
+  return knex.schema.hasTable('cases').then((exists) => {
     if (!exists) {
-      knex.schema.createTable('cases', (table) => {
+      return knex.schema.createTable('cases', (table) => {
         table.increments('id').primary();
         table.string('details', 500).notNullable();
         table.specificType('location', 'POINT').notNullable();
-        table.string('address', 300).notNullable();
+        table.string('address', 400).notNullable();
         table.string('image_url', 500).notNullable();
         table.integer('status').defaultTo(0);
         table.timestamp('created_at', { precision: 6 }).defaultTo(knex.fn.now(6));
