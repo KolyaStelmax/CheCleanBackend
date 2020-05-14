36255375bcec427d2350dd0fff7e9532ad2c21ae
Alexander Kretov <alexander.kretov99@gmail.com>
2020-05-14 18:48:52 +0300

    CC-15 updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,17 +2,19 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    client: 'postgres',
+    client: 'postgresql',
     connection: {
       host : process.env.PGHOST,
       user : process.env.PGUSER,
       password : process.env.PGPASSWORD,
       database: process.env.PGDATABASE,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   },
 };
-
-// console.log(process.env.PGUSER)
-// console.log(process.env.PGHOST)
-// console.log(process.env.PGPASSWORD)
-// console.log(process.env.PGDATABASE)
