02c83f6bf405d4c0c739a213bf9cd0c1ad4a1630
Dima Bilozor <swatdimonbil@gmail.com>
2020-05-16 14:32:54 +0300

    CC-54 fixed eslint and files

--- src/mocks/case.mocks.js
+++ src/mocks/case.mocks.js
@@ -1,29 +1,33 @@
 const newCaseMock = {
-	details: 'мусор',
-	location: { latitude: 49.457857, longitude: 32.043704 },
-	image_url: 'test-image'
+  details: 'мусор',
+  location: { latitude: 49.457857, longitude: 32.043704 },
+  image_url: 'test-image',
 };
 
 const casesMock = [
-	{
-		id: 1,
-		details: 'мусор',
-		location: { latitude: 49.457857, longitude: 32.043704, address: 'test_address', map_image_url: 'test-url' },
-		image_url: 'test-image',
-		status: 0,
-		created_at: '2020-05-03T14:09:38.244Z'
-	},
-	{
-		id: 2,
-		details: 'мусор2',
-		location: { latitude: 49.457857, longitude: 32.043704, address: 'test_address', map_image_url: 'test-url' },
-		image_url: 'test-image',
-		status: 0,
-		created_at: '2020-05-03T14:09:38.244Z'
-	}
+  {
+    id: 1,
+    details: 'мусор',
+    location: {
+      latitude: 49.457857, longitude: 32.043704, address: 'test_address', map_image_url: 'test-url',
+    },
+    image_url: 'test-image',
+    status: 0,
+    created_at: '2020-05-03T14:09:38.244Z',
+  },
+  {
+    id: 2,
+    details: 'мусор2',
+    location: {
+      latitude: 49.457857, longitude: 32.043704, address: 'test_address', map_image_url: 'test-url',
+    },
+    image_url: 'test-image',
+    status: 0,
+    created_at: '2020-05-03T14:09:38.244Z',
+  },
 ];
 
 module.exports = {
-	newCaseMock,
-	casesMock,
+  newCaseMock,
+  casesMock,
 };
--- src/mocks/common.js
+++ src/mocks/common.js
@@ -1,19 +1,19 @@
 const { casesMock, newCaseMock } = require('./case.mocks');
 
 class CommonMocks {
-	static get newCase() {
-		return this.getDeepCopy(newCaseMock);
-	}
+  static get newCase() {
+    return this.getDeepCopy(newCaseMock);
+  }
 
-	static get cases() {
-		return this.getDeepCopy(casesMock);
-	}
+  static get cases() {
+    return this.getDeepCopy(casesMock);
+  }
 
-	static getDeepCopy(mock) {
-		return JSON.parse(JSON.stringify(mock));
-	}
-};
+  static getDeepCopy(mock) {
+    return JSON.parse(JSON.stringify(mock));
+  }
+}
 
 module.exports = {
-	CommonMocks,
+  CommonMocks,
 };
